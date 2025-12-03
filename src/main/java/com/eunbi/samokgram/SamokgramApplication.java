b082aceb80efcdae20a3839fa79ca427398b53b8
DESKTOPG9EHDE8\Mega <megaIt@megaIT>
2025-12-03 18:48:17 +0900

    [이은비] - 사목그램 프로젝트 생성

---
+++ src/main/java/com/eunbi/samokgram/SamokgramApplication.java
@@ -0,0 +1,13 @@
+package com.eunbi.samokgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamokgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamokgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eunbi/samokgram/SamokgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunbi.samokgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamokgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
