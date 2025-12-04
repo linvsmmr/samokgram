92e44c87e6112ca5b4a1048a2942a04588e14aaf
samokgram(학원) <eb.lee@skuldlabs.co.kr>
2025-12-04 18:04:44 +0900

    [이은비] - 회원가입 개발 관련 설정

--- src/main/java/com/eunbi/samokgram/hello/HelloController.java
+++ src/main/java/com/eunbi/samokgram/hello/HelloController.java
@@ -1,7 +1,14 @@
 package com.eunbi.samokgram.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
 }
